0f09aaba752ca4e5840a480cdacc1f6cdce2a6b6
ihateindex <47144148+ihateindex@users.noreply.github.com>
2024-04-03 16:59:43 +0900

    chore: 전환이율 수정

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import DepositResult from './components/DepositResult/DepositResult';
 const INITIAL_DEPOSIT_DATA = {
     'default-deposit': '',
     'default-rent': '',
-    'conversion-interest-rate': '6',
+    'conversion-interest-rate': '7',
     'conversion-rate': '',
     'conversion-deposit': '',
     'conversion-rent': '',
@@ -29,7 +29,7 @@ function App() {
             setDepositData((prevData) => {
                 return {
                     ...prevData,
-                    ['conversion-interest-rate']: '6',
+                    ['conversion-interest-rate']: '7',
                 };
             });
         }
@@ -37,7 +37,7 @@ function App() {
             setDepositData((prevData) => {
                 return {
                     ...prevData,
-                    ['conversion-interest-rate']: '2.5',
+                    ['conversion-interest-rate']: '3.5',
                 };
             });
         }
